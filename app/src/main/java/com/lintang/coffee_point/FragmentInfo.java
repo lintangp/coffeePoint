e19ad7f59da832f8c34bb08f93bd9c602011d778
lintang pramesti <lintangpramesti77@gmail.com>
2023-06-16 15:39:24 +0700

    tambah splashscreen + login

---
+++ app/src/main/java/com/lintang/coffee_point/FragmentInfo.java
@@ -0,0 +1,6 @@
+package com.lintang.coffee_point;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentInfo extends Fragment {
+}
--- app/src/main/java/com/lintang/coffee_point/Login.java
+++ app/src/main/java/com/lintang/coffee_point/Login.java
@@ -1,14 +1,57 @@
 package com.lintang.coffee_point;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText etemail, etpassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        etemail  = findViewById(R.id.usernameEditText);
+        etpassword  = findViewById(R.id.passwordEditText);
+        Button login = findViewById(R.id.loginButton);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String email = etemail.getText().toString();
+        String password = etpassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Login successful
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(Login.this, "Authentication success.", Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(Login.this, MainActivity.class);
+                            startActivity(i);
+                            // Proceed with your desired actions, such as navigating to the main activity
+                        } else {
+                            // Login failed
+                            Toast.makeText(Login.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
