793083abe9c3b0abb7ea484f796a8121fc8fadec
lintang pramesti <lintangpramesti77@gmail.com>
2023-06-21 20:39:32 +0700

    bikin ambil data info dari api

--- app/src/main/java/com/lintang/coffee_point/FragmentInfo.java
+++ app/src/main/java/com/lintang/coffee_point/FragmentInfo.java
@@ -1,15 +1,24 @@
 package com.lintang.coffee_point;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -21,44 +30,62 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class FragmentInfo extends Fragment implements OnMapReadyCallback {
     private MapView mapView;
     private GoogleMap map;
-    private TextView info;
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private TextView info, nama, tanggal ;
+    private RequestQueue requestQueue;
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_info, container, false);
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_info, container, false);
+    
+            mapView = rootView.findViewById(R.id.mapView);
+            mapView.onCreate(savedInstanceState);
+            mapView.getMapAsync(this);
+            info = rootView.findViewById(R.id.textinfo);
+            nama = rootView.findViewById(R.id.textJudul);
+            tanggal = rootView.findViewById(R.id.texttanggal);
 
-        mapView = rootView.findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(this);
-        info = rootView.findViewById(R.id.textinfo);
+            requestQueue = Volley.newRequestQueue(requireContext());
 
-        db.collection("infoRestaurant")
-                .document("info")
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        DocumentSnapshot document = task.getResult();
-                        if (document.exists()) {
-                            // Access document data here
-                            String value = document.getString("txtInfo");
-                            info.setText(value);
-                            // Process the retrieved data
-                        } else {
-                            // Document doesn't exist
+            String url = "http://192.168.207.148/coffee_point/";
+            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                    new Response.Listener<JSONArray>() {
+                        @Override
+                        public void onResponse(JSONArray response) {
+                            try {
+                                // Assuming the response is a JSON array containing a single object
+                                JSONObject jsonObject = response.getJSONObject(0);
+                                String namacafe = jsonObject.getString("nama");
+                                String tanggalBerdiri = jsonObject.getString("tanggalBerdiri");
+                                String deskripsi = jsonObject.getString("deskripsi");
+
+                                nama.setText(namacafe);
+                                tanggal.setText(tanggalBerdiri);
+                                info.setText(deskripsi);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
                         }
-                    } else {
-                        // Handle the error
-                    }
-                });
-        return rootView;
-
-    }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            String errorMessage = error.getMessage();
+                            Toast.makeText(requireContext(), "Failed to retrieve data from API: " + errorMessage, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+            requestQueue.add(jsonArrayRequest);
+            return rootView;
+        }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
